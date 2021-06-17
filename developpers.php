13e3b0191a60550c846bd89d33aba9af82024af9
LHP6jlamanu <81558351+LHP6jlamanu@users.noreply.github.com>
2021-06-17 14:44:42 +0200

    card

---
+++ developpers.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"> 
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="assets/style.css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <title>Developpers</title>
+
+</head>
+<body>
+
+    <div class="container">
+    <nav class="navbar navbar-expand-lg  border border-2 border-danger rounded text-danger">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Bonjour "John Do"</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Nos Célibataires</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inscription</a>
+        </li>
+        
+    </div>
+  </div>
+</nav>
+
+<div class="col-3 card mt-4" style="width: 18rem;">
+<!-- "assets/img/angelina.jpg" -->
+  <img src="exo paul 2/assets/img/femme.jpg" class="card-img-top p-2" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Angelina Jolie</h5>
+    <ul class="list-group list-group-flush">
+    <li class="list-group-item">54 ans</li>
+    <li class="list-group-item">Code postal</li>
+    <li class="list-group-item">Femme</li>
+    <li class="list-group-item">Homme</li>
+    <li class="list-group-item">Description</li>
+  </ul>
+    <a href="#" class="btn btn-primary">Like</a>
+  </div>
+</div>
+
+    </div>
+ 
+
+    <script>
+
+</script>
+</body>
+</html>
